0fdac5e8faa05242a1587c8c32e1f76088ea8fb2
akiTanak <akihito_tanaka@me.com>
2021-12-06 01:08:05 +0900

    練習問題 4.4 のベースを書いた。

--- chap_04/04_04/practice_04_04.go
+++ chap_04/04_04/practice_04_04.go
@@ -1,2 +1,18 @@
 // 一回のパスで操作を行うrotateを書く
 package main
+
+func main() {
+
+}
+
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i<j; i, j + i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func rotate(s []int, step int) {
+	reverse(s[:step])
+	reverse(s[step:])
+	reverse(s)
+}
