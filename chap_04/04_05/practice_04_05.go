078c2baf98844ef8254f9715d4cce3820ffbf1e3
akitanak <akihito_tanaka@me.com>
2021-12-06 23:32:24 +0900

    練習問題4.5に回答

--- chap_04/04_05/practice_04_05.go
+++ chap_04/04_05/practice_04_05.go
@@ -1,2 +1,31 @@
 // []stringスライス内で隣接している重複をスライス内で除去する関数を書く。
 package main
+
+import "fmt"
+
+func main() {
+	s := []string{"one", "two", "three", "three", "four", "five", "six", "six", "seven", "eight", "eight", "nine"}
+
+	s = removeDup(s)
+	fmt.Println(s)
+}
+
+func removeDup(s []string) []string {
+	var prev string
+	var dups []int
+	for i, v := range s {
+		if v == prev {
+			dups = append(dups, i)
+		}
+		prev = v
+	}
+	for i, dup := range dups {
+		s = remove(s, dup-i)
+	}
+	return s
+}
+
+func remove(s []string, i int) []string {
+	copy(s[i:], s[i+1:])
+	return s[:len(s)-1]
+}
