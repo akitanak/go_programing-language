7a07d68738523ac1b9eec15ad7a88c8467b08249
akitanak <akihito_tanaka@me.com>
2022-01-13 18:11:12 +0900

    練習問題4.6を回答

--- chap_04/04_06/practice_04_06.go
+++ chap_04/04_06/practice_04_06.go
@@ -3,14 +3,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"unicode"
 )
 
 func main() {
-	s := []byte("ab \tcdefg\nhijk\v\f lmnop\r\ns\u0085\t")
+	s := []byte("ab \tcdefg\nhijk\v\f \u00A0lmnop\r\ns\u0085\t")
 	fmt.Printf("original: %s\n", s)
 	s = compressSpace(s)
 	fmt.Printf("result: %s\n", s)
+	expected := "ab cdefg hijk lmnop s "
+	if string(s) != expected {
+		fmt.Printf("expected: %X, actual: %X\n", expected, s)
+		os.Exit(1)
+	}
 }
 
 func compressSpace(s []byte) []byte {
@@ -19,18 +25,21 @@ func compressSpace(s []byte) []byte {
 	// ひとまずunicodeスペースをASCIIスペースへ変換する。
 	for _, r := range s {
 		if unicode.IsSpace(rune(r)) {
+			// スペースの直前がC2だった場合は、C2を除去するためにインデックスを一つ戻す。
+			if i > 0 && s[i-1] == 0xc2 {
+				i--
+			}
 			if existStartSpace {
 				continue
 			} else {
-				// 2byte文字のスペース(u+0085, U+00A0)だった場合は、直前のbyteのC2を除去するためにindexを戻す。
-				if i > 0 && s[i-1] == 194 {
-					i--
-				}
 				s[i] = 32
 				existStartSpace = true
 			}
 		} else {
 			s[i] = r
+			if rune(r) == 0xc2 {
+				continue
+			}
 			existStartSpace = false
 		}
 		i++
