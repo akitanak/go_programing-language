2fdfc31cf4681b363b56c6569ee05f9d226c1439
akitanak <akihito_tanaka@me.com>
2022-01-14 10:11:23 +0900

    練習問題4.8に回答

--- chap_04/04_08/practice_04_08.go
+++ chap_04/04_08/practice_04_08.go
@@ -10,7 +10,20 @@ import (
 	"unicode/utf8"
 )
 
+type class string
+
+const (
+	Control = class("control")
+	Letter  = class("letter")
+	Mark    = class("mark")
+	Number  = class("number")
+	Punct   = class("puct")
+	Space   = class("space")
+	Symbol  = class("symbol")
+)
+
 func main() {
+	classCounts := make(map[class]int)
 	counts := make(map[rune]int)
 	var utflen [utf8.UTFMax + 1]int
 	invalid := 0
@@ -31,6 +44,12 @@ func main() {
 		}
 		counts[r]++
 		utflen[n]++
+		c, err := classificateRune(r)
+		if err == nil {
+			classCounts[c]++
+		} else {
+			fmt.Printf("charcount: %v\n", err)
+		}
 	}
 	fmt.Print("rune\tcount\n")
 	for c, n := range counts {
@@ -42,7 +61,32 @@ func main() {
 			fmt.Printf("%d\t%d\n", i, n)
 		}
 	}
+	fmt.Print("\nlen\tcount\n")
+	for c, n := range classCounts {
+		fmt.Printf("%v\t%d\n", c, n)
+	}
 	if invalid > 0 {
 		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
+
+func classificateRune(r rune) (class, error) {
+	switch {
+	case unicode.IsControl(r):
+		return Control, nil
+	case unicode.IsLetter(r):
+		return Letter, nil
+	case unicode.IsMark(r):
+		return Mark, nil
+	case unicode.IsNumber(r):
+		return Number, nil
+	case unicode.IsPunct(r):
+		return Punct, nil
+	case unicode.IsSpace(r):
+		return Space, nil
+	case unicode.IsSymbol(r):
+		return Symbol, nil
+	default:
+		return "", fmt.Errorf("%X is undefined class", r)
 	}
 }
