b74fb851d77174f56399527e822dc92d11a98465
akitanak <akihito_tanaka@me.com>
2022-01-14 10:59:20 +0900

    練習問題4.9に回答

--- chap_04/04_09/practice_04_09.go
+++ chap_04/04_09/practice_04_09.go
@@ -1,2 +1,48 @@
 // 入力テキストファイル内のそれぞれの単語の出現頻度を報告するプログラムwordfreqを書きなさい。入力を行ではなく単語へ分割するために、最初のScan呼び出しの前にinput.Split(bufio.ScanWords)を呼び出しなさい。
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		occurrences := wordfreq(os.Stdin)
+		printOccurrences(occurrences)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "wordfreq: %v\n", err)
+				continue
+			}
+			defer f.Close()
+
+			occurrences := wordfreq(f)
+			printOccurrences(occurrences)
+		}
+	}
+}
+
+func wordfreq(f *os.File) map[string]int {
+	occurrences := make(map[string]int)
+
+	input := bufio.NewScanner(f)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		occurrences[input.Text()]++
+	}
+
+	return occurrences
+}
+
+func printOccurrences(occurrences map[string]int) {
+	fmt.Println("word\tcount")
+	for w, count := range occurrences {
+		fmt.Printf("%v\t%d\n", w, count)
+	}
+}
