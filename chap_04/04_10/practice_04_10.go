da6fd8f173b69f9c8e801cb51545406354100cca
akitanak <akihito_tanaka@me.com>
2022-01-14 13:44:05 +0900

    練習問題4.10に回答

--- chap_04/04_10/practice_04_10.go
+++ chap_04/04_10/practice_04_10.go
@@ -5,8 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"gopl.io/ch4/github"
+)
+
+const (
+	lessThanMonth = "lessThanMonth"
+	lessThanYear  = "lessThanYear"
+	moreThanYear  = "moreThanYear"
 )
 
 func main() {
@@ -14,9 +21,38 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	classified := classifiedIssuesByCreatedDate(&(result.Items))
+
 	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
+	for class, issues := range classified {
+		fmt.Printf("\n%v: %d issues\n", class, len(issues))
+		printIssues(&issues)
+	}
+}
+
+func classifiedIssuesByCreatedDate(issues *[]*github.Issue) map[string][]*github.Issue {
+	classified := make(map[string][]*github.Issue)
+	classified[lessThanMonth] = make([]*github.Issue, 0)
+	classified[lessThanYear] = make([]*github.Issue, 0)
+	classified[moreThanYear] = make([]*github.Issue, 0)
+	currentTime := time.Now().UTC()
+	for _, issue := range *issues {
+		switch {
+		case issue.CreatedAt.After(currentTime.Add(-(time.Hour * 24 * 30))):
+			classified[lessThanMonth] = append(classified[lessThanMonth], issue)
+		case issue.CreatedAt.After(currentTime.Add(-(time.Hour * 24 * 365))):
+			classified[lessThanYear] = append(classified[lessThanYear], issue)
+		default:
+			classified[moreThanYear] = append(classified[moreThanYear], issue)
+		}
+	}
+
+	return classified
+}
+
+func printIssues(issues *[]*github.Issue) {
+	for _, issue := range *issues {
 		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+			issue.Number, issue.User.Login, issue.Title)
 	}
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ replace local.packages/chap_02/popcount => ./chap_02/popcount
 replace local.packages/chap_02/tempconv => ./chap_02/tempconv
 
 require (
+	gopl.io v0.0.0-20211004154805-1ae3ec64947b // indirect
 	local.packages/chap_02/popcount v0.0.0-00010101000000-000000000000 // indirect
 	local.packages/chap_02/tempconv v0.0.0-00010101000000-000000000000 // indirect
 )
